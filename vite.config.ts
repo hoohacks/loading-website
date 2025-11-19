89fb11103da5c90876f1692efd04d892fe98fb9c
Diana Lin <lindiana1206@gmail.com>
2025-11-19 18:40:10 -0500

    remove module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,14 @@
-import { defineConfig, type Plugin } from 'vite';
-import { federation } from '@module-federation/vite';
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import { vercelToolbar } from '@vercel/toolbar/plugins/vite';
 import react from '@vitejs/plugin-react';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    vercelToolbar(),
     react(),
-    federation({
-      name: 'root',
-      manifest: true,
-      remotes: {
-        content: {
-          type: 'module',
-          name: 'content',
-          entry: '/_content/remoteEntry.js',
-        },
-      },
-      shared: {
-        react: {
-          singleton: true,
-        },
-        'react/': {
-          singleton: true,
-        },
-        'react-dom': {
-          singleton: true,
-        },
-        'react-dom/': {
-          singleton: true,
-        },
-      },
-    }) as Plugin[],
   ],
   build: {
     target: 'chrome89',
-  },
+  }
 });
