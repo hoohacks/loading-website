0b59d2987eac74c99c3a25d19727d8940c888a5a
Diana Lin <lindiana1206@gmail.com>
2025-11-19 18:24:42 -0500

    remove turbo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, type Plugin } from 'vite';
 import { federation } from '@module-federation/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { microfrontends } from '@vercel/microfrontends/experimental/vite';
 import { vercelToolbar } from '@vercel/toolbar/plugins/vite';
 import react from '@vitejs/plugin-react';
 
@@ -9,7 +8,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    microfrontends() as Plugin,
     vercelToolbar(),
     react(),
     federation({
